e8c60a53eeb061f22219f3df813028a059f75a81
arko-aesd <arko9699@gmail.com>
2025-06-24 20:32:34 +0530

    assignment-5-part-2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 7b77e4603f8c0bb72f7c436f7e4d518c27393c60
+AESD_ASSIGNMENTS_VERSION = 71d98e61799f600efeae5d996ab8a60ba1317406
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
